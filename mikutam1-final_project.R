0462938e4410b3e6d2cc614fcb7be8e3275c93a0
mikutam1 <mikutam1@tcnj.edu>
2025-12-04 00:27:46 -0500

    Add files via upload

---
+++ mikutam1-final_project.R
@@ -0,0 +1,20 @@
+## Load packages
+# NOTE: Run base.R if these commands return an error!
+library(readr)
+
+
+# set working directory
+setwd("/courses/STA145/mikutam1")
+
+# Load the data
+data <- read_delim("data.csv")
+
+table(data$rachel_mc)
+summary(data$rachel_mc)
+
+mean(data$cp_visit, na.rm = TRUE)
+sd(data$cp_visit, na.rm = TRUE)
+summary(data$cp_visit)
+
+tapply(data$cp_visit, data$rachel_mc, mean)
+tapply(data$cp_visit, data$rachel_mc, sd)
